fee145547cff36035ad5927a3379b09bfea5b88a
Shakir <shakirk72@gmail.com>
2022-01-13 16:04:09 +0530

    module 1 commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import Head from "next/head"
+
+export const Header = (props) => {
+    return <Head>
+    <title>{props.title}</title>
+    <meta name="description" content={props.description} />
+    <meta name="keywords" content={props.keywords} />
+    </Head>
+}
+
+// export default Header;
+
+export const Header2 = ({title, description, keywords}) => {
+    return <Head>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    <meta name="keywords" content={keywords} />
+    </Head>
+}
+
